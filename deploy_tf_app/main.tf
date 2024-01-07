f5ed609125553055d25c55124693c6d0ee60e6f2
edencoania <edencoania@gmail.com>
2024-01-07 17:51:13 +0200

    aaacda

--- deploy_tf_app/main.tf
+++ deploy_tf_app/main.tf
@@ -45,25 +45,25 @@ resource "aws_instance" "app_server" {
       host        = aws_instance.app_server.public_ip  
     } 
   }
+}
+provisioner "null_resource" "local_provisioner" {
+    
+  triggers = {
+    instance_id = aws_instance.app_server.id
+  }
 
-      provisioner "remote-exec" {
-    inline = [
-"sudo apt update -y" ,
-"sudo apt install docker.io -y", 
-"sudo systemctl enable docker" ,
-"sudo apt install python3 -y" ,
-"sudo apt install pip -y" ,
-"pip install flask" ,
-"sudo docker pull edencoania/release:hello_actions-latest" ,
-"python3 /home/ubuntu/app.py",
-    ]
 
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("weather.pem")
-      host        = aws_instance.app_server.public_ip
-    }
+  provisioner "local-exec" {
+    command = <<EOT
+      ssh -i weather.pem ubuntu@${aws_instance.app_server.public_ip} "sudo apt update -y \
+      && sudo apt install docker.io -y \
+      && sudo systemctl enable docker \
+      && sudo apt install python3 -y \
+      && sudo apt install python3-pip -y \
+      && pip3 install flask \
+      && sudo docker pull edencoania/release:hello_actions-latest \
+      && python3 /home/ubuntu/app.py"
+    EOT
   }
 }
 
